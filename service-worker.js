68737e0c31fc78da4c296e9bbe3c072f3f0a0b94
Leandro Aragão <leandroaragao28@gmail.com>
2026-06-20 17:18:03 -0300

    Versao mais visivel: move do rodape para o cabecalho
    - index: tag de versao no header, abaixo do status ao vivo
    - ficha: ao lado do Voltar. Placeholder ja visivel, vira versao vXX toque para checar
    - SW v15 para v16

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Trocar o VERSION muda este arquivo → o navegador detecta a nova versão
  * e o js/pwa.js mostra o banner "Nova versão disponível".
  */
-const VERSION = 'v15';
+const VERSION = 'v16';
 const CACHE = 'onze-dias-' + VERSION;
 
 // Caminhos relativos ao escopo (raiz do site).
