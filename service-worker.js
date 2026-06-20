adacac4daca789277605986fad2fcc98e90d46c5
Leandro Aragão <leandroaragao28@gmail.com>
2026-06-20 14:03:50 -0300

    Corrige login: reverte authDomain para firebaseapp.com (redirect_uri_mismatch)
    O authDomain=web.app causava redirect_uri_mismatch: o cliente OAuth do Google
    so autoriza .../firebaseapp.com/__/auth/handler. Volta para firebaseapp.com
    (handler autorizado, config que funcionou para o Eduardo). Popup mantido. SW v12.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Trocar o VERSION muda este arquivo → o navegador detecta a nova versão
  * e o js/pwa.js mostra o banner "Nova versão disponível".
  */
-const VERSION = 'v11';
+const VERSION = 'v12';
 const CACHE = 'onze-dias-' + VERSION;
 
 // Caminhos relativos ao escopo (raiz do site).
